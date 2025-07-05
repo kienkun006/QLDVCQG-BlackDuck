32dfe511e8598b75d077f008335d37a6413314ca
KIEU TRUNG KIEN <kieutrungkien1382006@gmail.com>
2025-07-06 03:40:11 +0700

    Update AppRoute.js

--- Backend/routes/AppRoute.js
+++ Backend/routes/AppRoute.js
@@ -5,6 +5,9 @@ import usersRoute from './usersRoute.js';
 import hokhauRoute from './hokhauRoute.js';
 import donviqlRoute from './donviqlRoute.js';
 import skqlRoute from './skqlRoute.js';
+import loginRoute from './loginRoute.js';
+import bhytRoute from './bhytRoute.js';
+
 export function AppRoute(app) {
     const router = express.Router();
 
@@ -12,8 +15,10 @@ export function AppRoute(app) {
     router.use('/hokhau', hokhauRoute);
     router.use('/phieu_dangky', phieuDangKyRoute);
     router.use('/users', usersRoute); 
-    router.use('/donviql',donviqlRoute);
-    router.use('/sukien',skqlRoute);
+    router.use('/donviql', donviqlRoute);
+    router.use('/sukien', skqlRoute);
+    router.use('/', loginRoute);
+    router.use('/bhyt', bhytRoute); 
 
     app.use('/api', router);
 }
